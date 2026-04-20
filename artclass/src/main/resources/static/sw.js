b0243198b825613e1e464b72ef43802885403431
rushadgaidhane <rushadigaidhane@gmail.com>
2026-04-20 06:06:10 +0530

    app

---
+++ artclass/src/main/resources/static/sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e => e.waitUntil(caches.open('artclass-v1').then(c => c.addAll(['/dashboard']))));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
